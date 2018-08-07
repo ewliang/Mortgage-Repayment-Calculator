ef46483f853fd8d15b34f224efbe6de7403b05ea
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-07 09:40:39 -0700

    Refactored for live update, added automatic commas for final value, and made sure everything works properly.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,18 @@
 var principal = document.getElementById('principal');
 var interestRate = document.getElementById('interestRate');
 var repaymentPeriod = document.getElementById('repaymentPeriod');
-var monthlyPayment = document.getElementById('monthlyRepayment');
+var monthlyRepayment = document.getElementById('monthlyRepayment');
 
-var monthlyPayment = function () {
-  if(principal.value && interestRate.value && repaymentPeriod.value) {
-    // Form Fields Filled
-    // 1. Convert interest rate from % to decimal.
-    var decimalInterestRate = parseFloat(interestRate.value) / 100;
-    // 2. Convert to monthly interest rate.
-    var monthlyInterestRate = decimalInterestRate / 12;
-    // 3. Increment by 1.
-    var incrementedMonthlyInterestRate = monthlyInterestRate + 1;
-    // 4. Number of Monthly Payments
-    var numberOfMonthlyPayments = repaymentPeriod * 12;
-    // 5. Calculate Monthly Payment
-    monthlyPayment.innerHTML = (monthlyInterest / (1 - Math.pow(incrementedMonthlyInterestRate, (-1 * numberOfMonthlyPayments)))) * principal.value;
-  } else {
-    // Please fill out all the form fields...
-  }
+function MonthlyPayment () {
+  // Form Fields Filled
+  // 1. Convert interest rate from % to decimal.
+  var decimalInterestRate = parseFloat(interestRate.value) / 100;
+  // 2. Convert to monthly interest rate.
+  var monthlyInterestRate = decimalInterestRate / 12;
+  // 3. Increment by 1.
+  var incrementedMonthlyInterestRate = monthlyInterestRate + 1;
+  // 4. Number of Monthly Payments
+  var numberOfMonthlyPayments = repaymentPeriod.value * 12;
+  // 5. Calculate Monthly Payment
+  monthlyRepayment.innerHTML = '$' + Math.ceil((monthlyInterestRate / (1 - Math.pow(incrementedMonthlyInterestRate, (-1 * numberOfMonthlyPayments)))) * principal.value).toLocaleString();
 }
